5e7565874c03ff62b2e96d034a6f5ace3c020ad0
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-11-18 10:57:56 +0100

    feat: add React Router setup with basic route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import {Routes, Route} from 'react-router-dom';
 import {Home} from '@/views';
 
 export default function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { GlobalUIProvider } from "@/context/GlobalUIContext";
+import {BrowserRouter} from 'react-router-dom';
+import {GlobalUIProvider} from '@/context/GlobalUIContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GlobalUIProvider>
-      <App />
-    </GlobalUIProvider>
+    <BrowserRouter>
+      <GlobalUIProvider>
+        <App />
+      </GlobalUIProvider>
+    </BrowserRouter>
   </StrictMode>
 );
 
