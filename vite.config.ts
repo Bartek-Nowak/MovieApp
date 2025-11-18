a4aaa91dba56bb84ebda02e2b8172d60dbcfaf85
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-11-18 13:37:26 +0100

    feat: add Vitest for unit testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+/// <reference types="vitest/config" />
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,9 +14,15 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setupTests.ts',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-})
+});
+
