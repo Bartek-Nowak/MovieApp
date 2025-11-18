98b3695af3a06990b5306d6515069834942a96a6
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-11-18 11:53:41 +0100

    feat(context): wrap main with MovieProvider for global movie state

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+import {GlobalUIProvider, MovieProvider} from '@/context';
 import './index.css';
 import App from './App.tsx';
-import {BrowserRouter} from 'react-router-dom';
-import {GlobalUIProvider} from '@/context/GlobalUIContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <GlobalUIProvider>
-        <App />
+        <MovieProvider>
+          <App />
+        </MovieProvider>
       </GlobalUIProvider>
     </BrowserRouter>
   </StrictMode>
